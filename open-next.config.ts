689c04999fe3b320b3f0df7d5e5e8a05723c45c8
NikhithReddySDSU <nikhithreddy@sdsu.edu>
2026-04-19 01:51:04 -0700

    fix(deploy): expanded open-next.config.ts with required middleware and cache overrides

--- open-next.config.ts
+++ open-next.config.ts
@@ -4,6 +4,17 @@ const config = {
       wrapper: "cloudflare-node",
       converter: "edge",
       proxyExternalRequest: "fetch",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
+  },
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
     },
   },
 };
